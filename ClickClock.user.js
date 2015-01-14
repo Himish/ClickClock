7f08156de3b84020958214cc098308724afb5c70
neonray <neonray@users.noreply.github.com>
2015-01-14 18:07:08 +0200

    Version change

--- ClickClock.user.js
+++ ClickClock.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            ClickClock
-// @version         1.2.4
+// @version         1.2.6
 // @description     Know your limits!
 // @author          Himish
 // @author          Kalabunga
